c93f3edffbe248536f6e74347c5a53c1a2cfb97d
AmanYL <ayilma6@montgomerycollege.edu>
2024-02-05 06:31:08 -0500

    Commiting Project 1 for CMSC204

--- Project1_CMSC204/src/DriverBalaeMekina.java
+++ Project1_CMSC204/src/DriverBalaeMekina.java
@@ -3,36 +3,28 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class DriverBalaeMekina {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws WeakPasswordException, LengthException, NoUpperAlphaException, NoLowerAlphaException, NoDigitException, NoSpecialCharacterException, InvalidSequenceException {
 		ArrayList<String> passwords;
 		String password1, password2;
 		
-		String[] p = {"334455BB#", "george2ZZZ#", "4Sal#", "bertha22", "august30", "a2cDe", 
-				"ApplesxxxYYzz#", "aa11Bb", "pilotProject", "AAAbb@123"};
-		passwords = new ArrayList<String>();
-		passwords.addAll(Arrays.asList(p)); // puts strings into the ArrayList
+		ArrayList<String> thePasswords;
+		String[] thePass = {"wALLlle@1275", "shR2!", "lwer28c@se", "UPER!!13CSE", "noNum@!QxZ", "They92Shar",
+						"Seeems90@GUD", "v@LId47"};
+		thePasswords = new ArrayList<>();
+		thePasswords.addAll(Arrays.asList(thePass));
 		
-			try{
-				 
-				boolean weakPwd = PasswordCheckerUtility.isWeakPassword("1234@aA");
-				assertTrue("Did not throw WeakPassword Exception",false);
-			}
-			catch(WeakPasswordException e)
-			{
-				System.out.println(e.getMessage());
-				assertTrue("Successfully threw a NoLowerAlphaExcepetion",true);
-			}
-			catch(Exception e)
-			{
-				System.out.println(e.getMessage());
-			}
-			
-			ArrayList<String> results;
-			results = PasswordCheckerUtility.getInvalidPasswords(passwords);
-		}
+		passwords = PasswordCheckerUtility.getInvalidPasswords(thePasswords);
+		for(String paswz : passwords)
+			System.out.println(paswz);
 
-	}
+//		System.out.println(PasswordCheckerUtility.isWeakPassword(thePasswords.get(2)));
+	}			
+}
+
+
 
  
--- Project1_CMSC204/src/PasswordCheckerTest_STUDENT.java
+++ Project1_CMSC204/src/PasswordCheckerTest_STUDENT.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * STUDENT tests for the methods of PasswordChecker
@@ -15,35 +17,43 @@ import org.junit.Test;
  *
  */
 public class PasswordCheckerTest_STUDENT {
-
+	ArrayList<String> thePasswords;
+	
 	@Before
 	public void setUp() throws Exception {
-		
+		String[] thePass = {"wALLlle@1275", "shR2!", "lwer28c@se", "UPER!!13CSE", "noNum@!QxZ", "They92Shar",
+				"Seeems90@GUD", "v@LId47"};
+		thePasswords = new ArrayList<>();
+		thePasswords.addAll(Arrays.asList(thePass));
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	
+		thePasswords = null;
 	}
 
 	/**
 	 * Test if the password is less than 6 characters long.
 	 * This test should throw a LengthException for second case.
+	 * @throws LengthException 
 	 */
 	@Test
-	public void testIsValidPasswordTooShort()
+	public void testIsValidPasswordTooShort() throws LengthException
 	{
-		fail("Not implemented by student yet");
+		assertTrue(PasswordCheckerUtility.isValidLength("wALLlle@1275"));
+		assertThrows(LengthException.class,()-> PasswordCheckerUtility.isValidLength("pqW4!"));
 	}
 	
 	/**
 	 * Test if the password has at least one uppercase alpha character
 	 * This test should throw a NoUpperAlphaException for second case
+	 * @throws NoUpperAlphaException 
 	 */
 	@Test
-	public void testIsValidPasswordNoUpperAlpha()
+	public void testIsValidPasswordNoUpperAlpha() throws NoUpperAlphaException
 	{
-		fail("Not implemented by student yet");
+		assertTrue(PasswordCheckerUtility.hasUpperAlpha("wALLlle@1275"));
+		assertThrows(NoUpperAlphaException.class, ()-> PasswordCheckerUtility.hasUpperAlpha("lwer28c@se"));
 	}
 	
 	/**
@@ -51,9 +61,10 @@ public class PasswordCheckerTest_STUDENT {
 	 * This test should throw a NoLowerAlphaException for second case
 	 */
 	@Test
-	public void testIsValidPasswordNoLowerAlpha()
+	public void testIsValidPasswordNoLowerAlpha() throws NoLowerAlphaException
 	{
-		fail("Not implemented by student yet");
+		assertTrue(PasswordCheckerUtility.hasLowerAlpha("wALLlle@1275"));
+		assertThrows(NoLowerAlphaException.class, ()-> PasswordCheckerUtility.hasLowerAlpha("UPER!!13CSE"));
 	}
 	/**
 	 * Test if the password has more than 2 of the same character in sequence
@@ -62,27 +73,46 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsWeakPassword()
 	{
-		fail("Not implemented by student yet");
+		try {
+			assertTrue(PasswordCheckerUtility.isWeakPassword("nAp1@5"));
+		} catch (WeakPasswordException e) {
+			assertTrue("Successfully threw a WeakPasswordExcepetion", true);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			assertTrue("An incorrect exception was thrown", false);
+		}
+		
+		try {
+			assertFalse(PasswordCheckerUtility.isWeakPassword("wALLlle@1275"));
+		} catch (WeakPasswordException | LengthException | NoUpperAlphaException | NoLowerAlphaException
+				| NoDigitException | NoSpecialCharacterException | InvalidSequenceException e) {
+			System.out.println(e.getMessage());
+			assertTrue("An exception was not suppossed to be thrown", false);
+		}
 	}
 	
 	/**
 	 * Test if the password has more than 2 of the same character in sequence
 	 * This test should throw a InvalidSequenceException for second case
+	 * @throws InvalidSequenceException 
 	 */
 	@Test
-	public void testIsValidPasswordInvalidSequence()
+	public void testIsValidPasswordInvalidSequence() throws InvalidSequenceException
 	{
-		fail("Not implemented by student yet");
+		assertFalse(PasswordCheckerUtility.NoSameCharInSequence("wALLlle@1275"));
+		assertThrows(InvalidSequenceException.class, ()-> PasswordCheckerUtility.NoSameCharInSequence("C@R245zzz"));
 	}
 	
 	/**
 	 * Test if the password has at least one digit
 	 * One test should throw a NoDigitException
+	 * @throws NoDigitException 
 	 */
 	@Test
-	public void testIsValidPasswordNoDigit()
+	public void testIsValidPasswordNoDigit() throws NoDigitException
 	{
-		fail("Not implemented by student yet");
+		assertTrue(PasswordCheckerUtility.hasDigit("wALLlle@1275"));
+		assertThrows(NoDigitException.class, ()-> PasswordCheckerUtility.hasDigit("noNum@!QxZ"));
 	}
 	
 	/**
@@ -92,7 +122,13 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordSuccessful()
 	{
-		fail("Not implemented by student yet");
+		try {
+			assertTrue(PasswordCheckerUtility.isValidPassword("wALLlle@1275"));
+		} catch (LengthException | NoUpperAlphaException | NoLowerAlphaException | NoDigitException
+				| NoSpecialCharacterException | InvalidSequenceException e) {
+			System.out.println(e.getMessage());
+			assertTrue("An exception was not supposed to be thrown", false);
+		}	
 	}
 	
 	/**
@@ -101,7 +137,39 @@ public class PasswordCheckerTest_STUDENT {
 	 */
 	@Test
 	public void testInvalidPasswords() {
-		fail("Not implemented by student yet");
+		ArrayList<String> results;
+		results = PasswordCheckerUtility.getInvalidPasswords(thePasswords);
+		
+		Scanner scan = new Scanner(results.get(0)); 
+		assertEquals(scan.next(), "shR2!");
+		String nextResults = scan.nextLine().toLowerCase();
+		assertTrue(nextResults.contains("long"));
+		
+		scan = new Scanner(results.get(1));  
+		assertEquals(scan.next(), "lwer28c@se");
+		nextResults = scan.nextLine().toLowerCase(); 
+		assertTrue(nextResults.contains("uppercase"));
+		 
+		scan = new Scanner(results.get(2));  
+		assertEquals(scan.next(), "UPER!!13CSE");
+		nextResults = scan.nextLine().toLowerCase();
+		assertTrue(nextResults.contains("lowercase"));
+		
+		scan = new Scanner(results.get(3));  
+		assertEquals(scan.next(), "noNum@!QxZ");
+		nextResults = scan.nextLine().toLowerCase();
+		assertTrue(nextResults.contains("digit"));
+		
+		scan = new Scanner(results.get(4));  
+		assertEquals(scan.next(), "They92Shar");
+		nextResults = scan.nextLine().toLowerCase();
+		assertTrue(nextResults.contains("special") );
+		
+		scan = new Scanner(results.get(5));  
+		assertEquals(scan.next(), "Seeems90@GUD");
+		nextResults = scan.nextLine().toLowerCase();
+		assertTrue(nextResults.contains("sequence") );
+		
 	}
 	
 }
